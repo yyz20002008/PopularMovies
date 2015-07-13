b4bee9bccf01315f70e47ad9452d1c9f29ff35b8
JamesYang <yyz20002008@gmail.com>
2015-07-13 19:49:14 -0400

    Initial commit

---
+++ app/src/main/java/com/yyz/android/popularmovies/MainActivityFragment.java
@@ -0,0 +1,37 @@
+package com.yyz.android.popularmovies;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView=inflater.inflate(R.layout.fragment_main, container, false);
+        GridView gridview = (GridView) rootView.findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(getActivity()));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+
+                Toast.makeText(getActivity(), position,Toast.LENGTH_SHORT).show();
+            }
+        });
+        return rootView;
+    }
+}
